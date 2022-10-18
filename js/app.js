92501600b9e671b815499684e15ca071b5e0ebff
awawpogi <vincentwilliamr@gmail.com>
2022-10-18 11:22:48 +0800

    Common header navigation

--- js/app.js
+++ js/app.js
@@ -10,3 +10,25 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden")
 hiddenElements.forEach((el) => observer.observe(el))
+
+
+// CUSTOM HTML ELEMENT
+
+class WebsiteNav extends HTMLElement {
+	connectedCallback(){
+		this.innerHTML = `
+<nav>
+  <a href="/index.html"><img src="/images/logo_transparent.png"></a>
+  <div class="nav-links">
+    <ul>
+      <li><a href="/index.html">Home</a></li>
+      <li><a href="">Projects</a></li>
+      <li><a href="">About Us</a></li>
+    </ul>
+  </div>
+</nav>
+`
+	}
+}
+
+customElements.define('website-nav', WebsiteNav)
